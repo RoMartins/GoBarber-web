6492cc6bca1f977d50f6475862cdf0d8c022577f
RoMartins <rodrigomartins_santos1804@hotmail.com>
2020-09-20 13:24:24 -0300

    Create SignUp page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import SignIn from './pages/Sign-in';
+import SignUp from './pages/Sign-up';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <SignIn></SignIn>
+    <SignUp />
   </>
 );
 
