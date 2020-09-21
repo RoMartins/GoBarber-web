95268e4b17755dbc901523ba226f98632444e0c8
RoMartins <rodrigomartins_santos1804@hotmail.com>
2020-09-21 00:38:27 -0300

    Input message errors implemented

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -6,8 +6,9 @@ import React, {
   useCallback,
 } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
 import { useField } from '@unform/core';
-import { Container } from './styles';
+import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -41,7 +42,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
 
   return (
     <>
-      <Container isFilled={isFilled} isfocused={isFocused}>
+      <Container isError={!!error} isFilled={isFilled} isfocused={isFocused}>
         {Icon && <Icon size={20} />}
         <input
           onFocus={handleInputFocus}
@@ -50,8 +51,12 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
           ref={inputRef}
           {...rest}
         />
+        {error && (
+          <Error title={error}>
+            <FiAlertCircle color="#c5303d" size={20} />
+          </Error>
+        )}
       </Container>
-      <div> {error}</div>
     </>
   );
 };
--- src/pages/Sign-in/index.tsx
+++ src/pages/Sign-in/index.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
+import validationError from '../../utils/getValidationErrors';
 
 import Logo from '../../assets/logo.svg';
 
@@ -8,32 +13,57 @@ import Input from '../../components/input';
 import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={Logo} alt="GoBarber" />
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-      <form>
-        <h1>Faça seu logon</h1>
+  const handleLogin = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
 
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
 
-        <Button type="submit">Entrar</Button>
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      const errors = validationError(error);
 
-        <a href="forgot">Esqueci minha senha </a>
-      </form>
-      <a href="">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Content>
+        <img src={Logo} alt="GoBarber" />
+
+        <Form ref={formRef} onSubmit={handleLogin}>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha </a>
+        </Form>
+        <a href="">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
 export default SignIn;
